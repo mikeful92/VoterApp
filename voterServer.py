ad56a6f55a781d4a15a94f5b24ecd41a3032eabd
Michael <mikeful92@hotmail.com>
2016-03-23 17:29:51 -0400

    Undo previous

--- voterServer.py
+++ voterServer.py
@@ -117,7 +117,7 @@ def queryGeneration(formData):
         else:
             month = "_%_%"
         for year in years:
-            yearQuery.append('BirthDate LIKE "' + month + '/_%_%/' + years + '"')
+            yearQuery.append('BirthDate LIKE "' + month + '/_%_%/' + year + '"')
         queryFields.append('(' + ' OR '.join(yearQuery) + ')')
 
     else:
